5e6480c5ee4462fd891d616ce3734bff65a103e7
ChrisHinde <chris@hindefjord.se>
2023-10-23 12:21:10 +0000

    Refactor variable name

--- cron/pub_sensors.py
+++ cron/pub_sensors.py
@@ -10,10 +10,10 @@ sens = subprocess.run(['sensors'], capture_output=True, text=True)
 
 for line in sens.stdout.splitlines():
     if line.find('C ') > 0:
-        f = re.search(r'([\w\s]+):\s+(\+[0-9]+[.][0-9]+)', line)
+        s = re.search(r'([\w\s]+):\s+(\+[0-9]+[.][0-9]+)', line)
 
-        id = f.groups()[0]
-        temp = f.groups()[1]
+        id = s.groups()[0]
+        temp = s.groups()[1]
 
         id = re.sub(r'(\s+[id]*\s?)', '_', id)
 
