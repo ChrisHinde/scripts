ca7d612e8ac539abcabf7c4226ad8c73da22acb2
ChrisHinde <chris@hindefjord.se>
2023-10-25 09:45:14 +0000

    Fix uptime publishing script

--- cron/pub_uptime.py
+++ cron/pub_uptime.py
@@ -10,12 +10,12 @@ topic_upt = "system/stats/uptime"
 
 up = subprocess.run(['uptime'], capture_output=True, text=True)
 
-u = re.search(r'up (.*), (\d+) users,[\w\s]+:\s+([0-9]+[.][0-9]+)*,\s+([0-9]+[.][0-9]+)*,\s+([0-9]+[.][0-9]+)*', up.stdout)
+u = re.search(r'up (.*),\s+(\d+) users,[\w\s]+:\s+([0-9]+[.][0-9]+)*,\s+([0-9]+[.][0-9]+)*,\s+([0-9]+[.][0-9]+)*', up.stdout)
 
 if u is not None:
     grps = u.groups()
 
-    upt = { 'uptime': grps[0], 'users': grps[1] }
+    upt = { 'uptime': grps[0].replace('  ',' '), 'users': grps[1] }
     usg = { '1min': grps[2], '5min': grps[3], '15min': grps[4] }
 
     msg = json.dumps(usg)
@@ -23,3 +23,6 @@ if u is not None:
 
     msg = json.dumps(upt)
     subprocess.run([cmd,'-t',topic_upt,'-m',msg])
+else:
+    print("Uptime ERROR")
+    exit(1)
