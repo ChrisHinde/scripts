2179164ff568d8f8820a806239f9e0f28ff25c8a
ChrisHinde <chris@hindefjord.se>
2023-10-26 10:32:59 +0000

    Add script to update apt and pub available updates

---
+++ cron/chk_pub_update.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/pyth
+
+import subprocess
+
+import os, sys
+import datetime, time
+
+cmd = "mosquitto_pub"
+topic = "system/updates/apt/"
+
+update_apt = False
+output_upd = False
+
+if os.geteuid() == 0:
+    update_apt = True
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--dont-update':
+        update_apt = False
+    if sys.argv[1] == '-v':
+        output_upd = True
+
+if update_apt:
+    upd = subprocess.run(['apt','update'], capture_output=True, text=True)
+
+    if output_upd:
+        print(upd.stdout)
+
+    os.environ['TZ'] = 'Europe/Stockholm'
+    time.tzset()
+    subprocess.run([cmd,'-t',topic+'last_updated','-m',str(datetime.datetime.now())])
+
+chk = subprocess.run(['/usr/lib/update-notifier/apt-check'], capture_output=True, text=True)
+
+(reg,sec) = chk.stderr.split(';')
+
+subprocess.run([cmd,'-t',topic+'regular','-m',reg])
+subprocess.run([cmd,'-t',topic+'security','-m',sec])
