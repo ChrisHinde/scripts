bfd0d13057b8650b96b66f5651afd158b2df9f5b
Chris Hindefjord <chris@hindefjord.se>
2025-12-01 23:19:14 +0100

    Add Confg code to Update check

--- cron/chk_pub_update.py
+++ cron/chk_pub_update.py
@@ -5,20 +5,20 @@ import subprocess
 import os, sys
 import datetime, time
 
+from config import Config
+
+debug = False
+
 cmd = "mosquitto_pub"
-topic = "system/updates/apt/"
-reg = 0
-sec = None
+topic = Config.t('apt','updates/apt') + '/'
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
 
-update_apt = False
-force_no_upd = True
+update_apt = Config.v('apt.run_update', False)
+force_no_upd = Config.v('apt.force_no_update', True)
 output_upd = False
 
-retain = True
-
-retain_flag = '-r' if retain else ''
+retain = Config.v('apt.retain', True)
 
 if os.geteuid() == 0 and not force_no_upd:
     update_apt = True
@@ -32,6 +32,8 @@ if len(sys.argv) > 1:
         update_apt = True
     if sys.argv[1] == '-v':
         output_upd = True
+    if sys.argv[1] == '-d':
+        debug = True
 
 if update_apt:
     upd = subprocess.run(['apt','update'], capture_output=True, text=True)
@@ -43,6 +45,8 @@ if update_apt:
     time.tzset()
     subprocess.run([cmd,'-t',topic+'last_updated','-m',str(datetime.datetime.now()), retain_flag])
 
+reg = 0
+sec = None
 proc = '/usr/lib/update-notifier/apt-check'
 
 if os.path.isfile(proc):
@@ -53,8 +57,16 @@ else:
     chk = subprocess.run(proc, capture_output=True, text=True)
     reg = chk.stdout.strip()
 
+ex = [cmd,'-t',topic+'regular','-m',reg]
+if retain:
+    ex.append('-r')
+if debug:
+    print(ex)
 
-subprocess.run([cmd,'-t',topic+'regular','-m',reg, retain_flag])
+subprocess.run(ex)
 
 if sec is not None:
-    subprocess.run([cmd,'-t',topic+'security','-m',sec, retain_flag])
+    ex = [cmd,'-t',topic+'security','-m',sec]
+    if retain:
+        ex.append('-r')
+    subprocess.run(ex)
